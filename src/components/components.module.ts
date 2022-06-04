885c2180e3f163d209670bd67e817d46625f49a7
sh <sh@nekta.tech>
2022-06-04 15:55:53 +0300

    add side-bar with navigation link, fonts and reset.css

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,10 +1,16 @@
 import {NgModule} from "@angular/core";
-import {NavigationComponent} from "./navigation/navigation.component";
+import {AngularSvgIconModule} from "angular-svg-icon";
+import {HttpClientModule} from "@angular/common/http";
+import {SideBarComponent} from "./side-bar/side-bar.component";
+import {LinkComponent} from "./link/link.component";
 
 @NgModule({
-  imports: [],
-  declarations: [NavigationComponent],
-  exports: [NavigationComponent]
+  imports: [
+    HttpClientModule,
+    AngularSvgIconModule.forRoot()
+  ],
+  declarations: [SideBarComponent, LinkComponent],
+  exports: [SideBarComponent]
 })
 export class ComponentsModule {
 }
