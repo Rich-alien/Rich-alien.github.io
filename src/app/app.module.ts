07d98d3ab65cbc2ab79ddcb1d633f60727c6ba09
sh <sh@nekta.tech>
2022-06-04 13:12:51 +0300

    push start template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {PagesModel} from "../pages/pages.model";
+import {AppRoutingModule} from "./app.routing";
+import {ComponentsModule} from "../components/components.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModel,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/components/components.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from "@angular/core";
+import {NavigationComponent} from "./navigation/navigation.component";
+
+@NgModule({
+  imports: [],
+  declarations: [NavigationComponent],
+  exports: [NavigationComponent]
+})
+export class ComponentsModule {
+}
